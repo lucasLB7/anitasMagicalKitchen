5d4fd10c1b46935238f596b457450b23b83f2c61
Luka <plucaslambert@gmail.com>
2018-03-25 21:35:59 +0300

    still stuggling with the whole bootstrrap thing.. :p nways..

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,18 @@ $(document).ready(function() {
         $('#under-21').show();
       }
     });
+
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
