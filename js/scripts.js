b1fd741b0f5ad08f264cdf51b3c6636a3d1e238c
Luka <plucaslambert@gmail.com>
2018-03-26 20:38:33 +0300

    Final changes, far from perfect but meh.. :p

--- js/scripts.js
+++ js/scripts.js
@@ -9,21 +9,27 @@ $(document).ready(function() {
     });
 
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
 
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+    function openNav() {
+        document.getElementById("myNav").style.width = "100%";
     }
-  }
-}
+
+    function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+    }
+
+
+    function openCity(evt, cityName) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(cityName).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
